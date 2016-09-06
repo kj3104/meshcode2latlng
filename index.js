f7a1f5d4b0d1c511df5ac266fdf9823eff93c08a
luca3104 <s1210006@u-aizu.ac.jp>
2016-09-07 04:26:32 +0900

    add package.json and index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+var first = require('./lib/meshcode2LatLng').first;
+var second = require('./lib/meshcode2LatLng').second;
+var third = require('./lib/meshcode2LatLng').third;
+var half = require('./lib/meshcode2LatLng').half;
+var quater = require('./lib/meshcode2LatLng').quater;
