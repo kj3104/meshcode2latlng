567afc7e3ee8a5da587b5e3d05566135b64a098c
luca3104 <s1210006@u-aizu.ac.jp>
2016-09-07 04:32:19 +0900

    1.0.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-var first = require('./lib/meshcode2LatLng').first;
-var second = require('./lib/meshcode2LatLng').second;
-var third = require('./lib/meshcode2LatLng').third;
-var half = require('./lib/meshcode2LatLng').half;
-var quater = require('./lib/meshcode2LatLng').quater;
+module.exports.first = require('./lib/meshcode2LatLng').first;
+module.exports.second = require('./lib/meshcode2LatLng').second;
+module.exports.third = require('./lib/meshcode2LatLng').third;
+module.exports.half = require('./lib/meshcode2LatLng').half;
+module.exports.quater = require('./lib/meshcode2LatLng').quater;
