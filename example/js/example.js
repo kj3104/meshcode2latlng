f07efa513794a6ad1618c28ab724110b7918974d
luca3104 <s1210006@u-aizu.ac.jp>
2016-09-09 17:30:16 +0900

    fixed quater function

--- example/js/example.js
+++ example/js/example.js
@@ -9,69 +9,104 @@ function initMap() {
 }
 
 function drawMesh(){
-  var code = 5339;
-  var loc =  meshcode2latlng.first(code);
-  new google.maps.Rectangle({
-    strokeColor: '#ff0000',
-    fillColor: '#ff0000',
-    map: map,
-    bounds: {
-      south: loc.south,
-      west: loc.west,
-      north: loc.north,
-      east: loc.east
-    }
-  });
-  var code = 533900;
-  for(var i=0;i<8;i++){
-    var loc =  meshcode2latlng.second(code);
-    var rectangle = new google.maps.Rectangle({
-      strokeColor: '#00ffff',
-      fillColor: '#00ffff',
-      map: map,
-      bounds: {
-        south: loc.south,
-        west: loc.west,
-        north: loc.north,
-        east: loc.east
-      }
-    });
-    code = code + 11;
-  }
-  var code = 53394600;
-  for(var i=0;i<10;i++){
-    var loc =  meshcode2latlng.third(code);
-    var rectangle = new google.maps.Rectangle({
-      strokeColor: '#00ff00',
-      fillColor: '#00ff00',
-      map: map,
-      bounds: {
-        south: loc.south,
-        west: loc.west,
-        north: loc.north,
-        east: loc.east
-      }
-    });
-    code = code + 11;
-  }
-  var code = 533946121;
-  for(var i=0;i<4;i++){
-    var loc =  meshcode2latlng.half(code);
-    new google.maps.Rectangle({
-      strokeColor: '#0000ff',
-      fillColor: '#0000ff',
-      map: map,
-      bounds: {
-        south: loc.south,
-        west: loc.west,
-        north: loc.north,
-        east: loc.east
-      }
-    });
-    code = code + 1;
-  }
+  // var code = 5339;
+  // var loc =  meshcode2latlng.first(code);
+  // new google.maps.Rectangle({
+  //   strokeColor: '#ff0000',
+  //   fillColor: '#ff0000',
+  //   map: map,
+  //   bounds: {
+  //     south: loc.south,
+  //     west: loc.west,
+  //     north: loc.north,
+  //     east: loc.east
+  //   }
+  // });
+  // var code = 533900;
+  // for(var i=0;i<8;i++){
+  //   var loc =  meshcode2latlng.second(code);
+  //   var rectangle = new google.maps.Rectangle({
+  //     strokeColor: '#00ffff',
+  //     fillColor: '#00ffff',
+  //     map: map,
+  //     bounds: {
+  //       south: loc.south,
+  //       west: loc.west,
+  //       north: loc.north,
+  //       east: loc.east
+  //     }
+  //   });
+  //   code = code + 11;
+  // }
+  // var code = 53394600;
+  // for(var i=0;i<10;i++){
+  //   var loc =  meshcode2latlng.third(code);
+  //   var rectangle = new google.maps.Rectangle({
+  //     strokeColor: '#00ff00',
+  //     fillColor: '#00ff00',
+  //     map: map,
+  //     bounds: {
+  //       south: loc.south,
+  //       west: loc.west,
+  //       north: loc.north,
+  //       east: loc.east
+  //     }
+  //   });
+  //   code = code + 11;
+  // }
+  // var code = 533946121;
+  // for(var i=0;i<4;i++){
+  //   var loc =  meshcode2latlng.half(code);
+  //   new google.maps.Rectangle({
+  //     strokeColor: '#0000ff',
+  //     fillColor: '#0000ff',
+  //     map: map,
+  //     bounds: {
+  //       south: loc.south,
+  //       west: loc.west,
+  //       north: loc.north,
+  //       east: loc.east
+  //     }
+  //   });
+  //   code = code + 1;
+  // }
+	var code = 5339461311;
+	for(var i=0;i<4;i++){
+		console.log(code);
+		var loc =  meshcode2latlng.quater(code);
+		var rectangle = new google.maps.Rectangle({
+			strokeColor: '#ffff00',
+			fillColor: '#ffff00',
+			map: map,
+			bounds: {
+				south: loc.south,
+				west: loc.west,
+				north: loc.north,
+				east: loc.east
+			}
+		});
+		code = code + 1;
+	}
+	var code = 5339461321;
+	for(var i=0;i<4;i++){
+		console.log(code);
+		var loc =  meshcode2latlng.quater(code);
+		var rectangle = new google.maps.Rectangle({
+			strokeColor: '#ffff00',
+			fillColor: '#ffff00',
+			map: map,
+			bounds: {
+				south: loc.south,
+				west: loc.west,
+				north: loc.north,
+				east: loc.east
+			}
+		});
+		code = code + 1;
+	}
 	var code = 5339461331;
   for(var i=0;i<4;i++){
+		console.log(code);
   	var loc =  meshcode2latlng.quater(code);
   	var rectangle = new google.maps.Rectangle({
   		strokeColor: '#ffff00',
@@ -86,5 +121,22 @@ function drawMesh(){
   	});
     code = code + 1;
   }
+	var code = 5339461341;
+	for(var i=0;i<4;i++){
+		console.log(code);
+		var loc =  meshcode2latlng.quater(code);
+		var rectangle = new google.maps.Rectangle({
+			strokeColor: '#ffff00',
+			fillColor: '#ffff00',
+			map: map,
+			bounds: {
+				south: loc.south,
+				west: loc.west,
+				north: loc.north,
+				east: loc.east
+			}
+		});
+		code = code + 1;
+	}
 
 }
